4c7f824ee89719f28ce834b7efeb15b65cca2994
Sai Chen <sai.chen@dena.local>
2019-04-12 18:42:04 +0800

    cs:: recy ws/Message

---
+++ ws/Message.go
@@ -0,0 +1,44 @@
+package webservice
+
+import (
+	"log"
+)
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"to,omitempty"`
+	Content   string `json:"content,omitempty"`
+	Type	  string `json:"type,omitempty"`
+	PushHandler func (manager *ClientManager)
+}
+
+const (
+	MESSAGE_TYPE_TO_SINGLE_USER = "single"	//单发
+	MESSAGE_TYPE_TO_GROUP_USER  = "group"	//组发
+	MESSAGE_TYPE_TO_MANY_USER   = "many" 	//群发
+	MESSAGE_TYPE_TO_BROADCAST   = "broadcast" //广播
+)
+
+func (message *Message) DefaultPushHandler(manager *ClientManager) {
+	switch message.Type {
+	case MESSAGE_TYPE_TO_SINGLE_USER:
+		conn, ok := manager.clients[message.Recipient]
+		if !ok {
+			log.Println("Conn: message to single err: \n%v", message)
+			return
+		}
+		conn.send <- []byte(message.Content)
+	case MESSAGE_TYPE_TO_BROADCAST:
+		for _, conn := range manager.clients {
+			select {
+			case conn.send <- []byte(message.Content):
+			default:
+				manager.disconnected(conn)
+			}
+		}
+	}
+}
+
+func (message *Message) SetPushHandler (handler func(manager *ClientManager)()) {
+	message.PushHandler = handler
+}
